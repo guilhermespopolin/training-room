bf8a4ab3a686b4d88516f23138f1a90a0c3ed2c8
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-06-11 14:20:40 +0200

    styles(advice-generator): updated tailwindcss theme based on style guide

--- advice-generator/app/root.tsx
+++ advice-generator/app/root.tsx
@@ -16,7 +16,18 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap",
+  },
+  { rel: "stylesheet", href: styles },
+];
 
 export default function App() {
   return (
