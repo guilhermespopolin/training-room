714038c83f94a4fbed16b4f63988b4222801d7f1
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-06-15 23:08:04 +0200

    feat(advice-generator): added advice module

---
+++ advice-generator/app/modules/advice/advice.server.ts
@@ -0,0 +1,21 @@
+type Advice = {
+  id: number;
+  text: string;
+};
+
+type ResponseData = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+export async function getRandomAdvice(): Promise<Advice> {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const { slip }: ResponseData = await response.json();
+
+  return {
+    id: slip.id,
+    text: slip.advice,
+  };
+}
