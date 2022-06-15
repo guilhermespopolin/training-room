a09f2c5911b9ea95ecc1a4fa00379f75069aada5
Guilherme Popolin <guilhermespopolin@gmail.com>
2022-06-15 23:51:43 +0200

    styles(advice-generator): updated styles

--- advice-generator/app/components/AdviceCard/AdviceCard.tsx
+++ advice-generator/app/components/AdviceCard/AdviceCard.tsx
@@ -10,21 +10,56 @@ export function AdviceCard({ advice }: AdviceCardProps) {
   return (
     <main className="flex items-center justify-center px-4 h-screen bg-blue-bayoux-700">
       <Form method="get" action="/">
-        <div className="relative flex flex-col items-center max-w-sm px-6 py-8 rounded-lg shadow-2xl bg-blue-bayoux-600 text-botticelli-500 font-bold md:max-w-md">
-          <h1 className="mb-6 text-aquamarine text-xs tracking-widest uppercase">
+        <div
+          className={[
+            "relative",
+            "flex",
+            "flex-col",
+            "items-center",
+            "max-w-sm",
+            "px-6",
+            "py-8",
+            "rounded-lg",
+            "shadow-2xl",
+            "bg-blue-bayoux-600",
+            "text-botticelli-500",
+            "md:max-w-md",
+          ].join(" ")}
+        >
+          <h1
+            className={[
+              "mb-6",
+              "text-aquamarine",
+              "text-xs",
+              "tracking-[0.2em]",
+              "font-semibold",
+              "uppercase",
+            ].join(" ")}
+          >
             Advice #{advice.id}
           </h1>
-          <blockquote className="text-2xl text-center">
+          <blockquote className="text-2xl font-bold text-center">
             {advice.text}
           </blockquote>
           <img
             className="m-6 w-full"
             role="separator"
-            src="/images/pattern-divider.svg"
+            src="/images/pattern-divider-mobile.svg"
+            srcSet="/images/pattern-divider-mobile.svg 375w,
+                    /images/pattern-divider-desktop.svg 768w"
             alt=""
           />
           <button
-            className="absolute top-[calc(100%-theme(space.7))] aspect-square px-4 rounded-full bg-aquamarine"
+            className={[
+              "absolute",
+              "top-[calc(100%-theme(space.7))]",
+              "aspect-square",
+              "px-4",
+              "rounded-full",
+              "bg-aquamarine",
+              "transition-shadow",
+              "hocus:shadow-[0_0_24px_0_theme(colors.aquamarine.DEFAULT)]",
+            ].join(" ")}
             aria-label="Get a new advice"
             type="submit"
           >
